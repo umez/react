5671920ec6fae27d0acbf1d401a8cd3b3208c736
umez <umez_ph@yahoo.co.uk>
2019-07-16 21:05:02 +0530

    updates

--- src/actions/index.js
+++ src/actions/index.js
@@ -19,26 +19,27 @@ export const signOut = () => {
 export const createStream = formValues => async (dispatch, getState) => {
     const {userId} = getState().auth;
     const response = await streams.post('/streams', {...formValues, userId});
-    dispatch ({type: CREATE_STREAM, payload : response.data})
-    createBrowserHistory.push('/')
+    dispatch ({type: CREATE_STREAM, payload : response.data});
+    createBrowserHistory.push('/');
 }
 
 export const fetchStreams = () => async dispatch => {
     const response = await streams.get('/streams');
-    dispatch({type: FETCH_STREAMS, payload: response.data})
+    dispatch({type: FETCH_STREAMS, payload: response.data});
 }
 
 export const fetchStream = id => async dispatch => {
     const response = await streams.get(`/streams/${id}`);
-    dispatch({type: FETCH_STREAM, payload: response.data})
+    dispatch({type: FETCH_STREAM, payload: response.data});
 }
 
 export const editStream = (id, formValues) => async dispatch => {
-    const response = await streams.put(`/streams/${id}`, formValues)
-    dispatch({type: EDIT_STREAM, payload: response.data})
+    const response = await streams.patch(`/streams/${id}`, formValues);
+    dispatch({type: EDIT_STREAM, payload: response.data});
+    createBrowserHistory.push('/');
 }
 
 export const deleteStream = id => async dispatch => {
     await streams.delete(`/streams/${id}`);
-    dispatch({type: DELETE_STREAM, payload: id})
+    dispatch({type: DELETE_STREAM, payload: id});
 }
--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,25 +1,39 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
+
 
 
 class StreamEdit extends React.Component {
-
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id);
     }
 
-    render() {
+    onSubmit = formValues => {
+        this.props.editStream(this.props.match.params.id, formValues)
+      }
+    
+      render() {
+        console.log(this.props.stream)
         if(!this.props.stream) {
-            return <div>Loading...</div>
+            return null;
         }
         return (
-            <div>{this.props.stream.title}</div>
-        )
+         <div>
+           <h3>Edit a Stream</h3>
+           <StreamForm 
+            initialValues={_.pick(this.props.stream, 'title', 'description')} 
+            onSubmit={this.onSubmit} 
+           />
+         </div>
+        );
+      }
     }
-}
+
 
 const mapStateToProps = (state, ownProps) => {
     return { stream: state.streams[ownProps.match.params.id]}
 }
-export default connect(mapStateToProps, { fetchStream} )(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream} )(StreamEdit);
