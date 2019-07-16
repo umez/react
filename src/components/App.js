bd6e8e91b26df3ae83d16800c6903b4bcd5cf77f
umez <umez_ph@yahoo.co.uk>
2019-07-16 17:29:47 +0530

    updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,27 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 
 import Header from './header';
 import StreamList from './streams/StreamList';
 import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamShow from './streams/StreamShow';
+import createBrowserHistory from '../history';
 
 const App = () => {
   return (
     <div className="ui container">
-        <BrowserRouter>
+        <Router history={createBrowserHistory}>
           <div>
               <Header />
               <Route path="/" exact component={StreamList} />
               <Route path="/streams/new" exact component={StreamCreate} />
+              <Route path="/streams/edit/:id" exact component={StreamEdit} />
+              <Route path="/streams/delete" exact component={StreamDelete} />
+              <Route path="/streams/show" exact compont={StreamShow} />
           </div>
-        </BrowserRouter>
+        </Router>
     </div>
   );
 };
